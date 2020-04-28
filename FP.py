badca8f55da73b9e4905d176dbd541c5967cd7c2
Matt <63558223+mcline1980@users.noreply.github.com>
2020-04-28 14:08:49 -0400

    Revert "Merge pull request #1 from mcline1980/datetime_conversion"
    This reverts commit 49047701fdb976eef52c392082baf8641391e782, reversing
    changes made to a9cb86dd49f7571cf06d253a2151e53454c0c1c7.

--- FP.py
+++ FP.py
@@ -2,13 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import openpyxl
 import os
-from datetime import datetime
 
 # Import data frames using Pandas
-# df_pd1 <- Presence Detected, exported from Google Drive as PD.csv
-# df_pnd1 <- Presence no longer Detected, exported from Google Drive as PND.csv
-# If This Then That (IFTTT) creates two unique spreadsheets in Google Drive
-# this process is a work around to get both data sets into a single data frame to manipulate.
+# df_pd1 <- Presence Detected
+# df_pnd1 <- Presence no longer Detected
+# If This Then That creates two unique spreadsheets in Google Drive and does not allow for merging.
+# this process is a work around to get both data sets into a single object to manipulate.
 
 df_pd1 = pd.read_csv('PD.csv', header=None)
 df_pd1.columns = ["Date", "Device"]
@@ -21,29 +20,9 @@ df_pd2['Presence'] = "Home"
 df_pnd2 = df_pnd1
 df_pnd2['Presence'] = "Away"
 
-# Create the final DF to include both Home/Away statuses and retain indexing
-df_p = df_pd2.append(df_pnd2, ignore_index=True)
-
-
-# DT CONVERT 1
-# Date and Time conversion function for the format used by IFTTT Samsung Smartthings presence detection
-# to Google Sheet applet
-def dt_convert_1(s2c):
-    s2c = datetime.strptime(s, '%B %d, %Y at %I:%M%p')
-    return s2c
-
-# Add new column to DF to account for normalized date and normalized time information
-# Use strptime to parse the date string with the following args:: '%B %d, %Y at %I:%M%p'
-# Iterate over all rows in the df_p data frame, extract the string from the 'Date' column
-# Run strptime against the new variable
-# Drop the new datetime variable into the correct location in the data frame using LOC
-loc = 0
-for i in df_p.iterrows():
-    s = df_p.loc[loc, 'Date']
-    # s = datetime.strptime(s, '%B %d, %Y at %I:%M%p')
-    s = dt_convert_1(s)
-    df_p.loc[loc, 'Normalized_Time'] = s
-    loc = loc + 1
+# Create new DF to include both Home/Away statuses
+frames = [df_pd2, df_pnd2]
+df_p = pd.concat(frames)
 
 
 # Return Unique Devices
@@ -59,11 +38,8 @@ def write_excel_file(write_file, file_name):
 
 
 # Summary Statistics
-def summary_stats(request):
-    if request == 'Device':
-        print("Presence count by device type:")
-        presence_count = df_p.groupby('Device')['Presence'].value_counts()
-        print(presence_count)
+print("Presence count by device type:")
+presence_count = df_p.groupby('Device')['Presence'].value_counts()
+print(presence_count)
+print(" ")
 
-
-write_excel_file(df_p, 'test_output')
